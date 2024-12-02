f99f90e4fea7ea34d1c2592fd6ffac14d764a71a
MdAnayatullah <mdanayatullah143@gmail.com>
2024-12-03 00:49:56 +0530

    Currency Converter Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import './App.css'
-import BackgroundChanger from './components/BackgroundChanger.jsx';
-import PasswordGenerator from './components/PasswordGenerator.jsx';
+import "./App.css";
+import BackgroundChanger from "./components/BackgroundChanger.jsx";
+import CurrencyConverter from "./components/CurrencyConverter.jsx";
+import PasswordGenerator from "./components/PasswordGenerator.jsx";
 
 function App() {
-  
-
   return (
     <>
-    {/* <BackgroundChanger/> */}
-    <PasswordGenerator/>
+      {/* <BackgroundChanger/> */}
+      {/* <PasswordGenerator /> */}
+      <CurrencyConverter />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    // fetch(
+    //   `https://v6.exchangerate-api.com/v6/865bae9b5b9e4fa22e51a8a2/latest/${currency}.json`
+    // )
+    fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+    console.log(data);
+  }, [currency]);
+  console.log(data);
+  return data;
+}
+
+export default useCurrencyInfo;
